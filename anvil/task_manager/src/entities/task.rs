caa37c4adbf460397a06a587720c4323e9d80a12
SinofPride-999 <jhaycodes999@gmail.com>
2026-02-16 23:11:48 +0000

    feat(anvil/task_manager): task entity

---
+++ anvil/task_manager/src/entities/task.rs
@@ -0,0 +1,18 @@
+use sea_orm::entity::prelude::*;
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
+#[sea_orm(table_name = "tasks")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i32,
+    pub title: String,
+    pub description: Option<String>,
+    pub status: String,
+    pub created_at: DateTimeUtc,
+    pub updated_at: DateTimeUtc,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
