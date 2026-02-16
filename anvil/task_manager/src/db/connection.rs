5b11b4ee3ba0a1f6eae59a05a0771272c744e9a8
SinofPride-999 <jhaycodes999@gmail.com>
2026-02-16 23:21:08 +0000

    feat(anvil/task_manager): use the value from YAML

--- anvil/task_manager/src/db/connection.rs
+++ anvil/task_manager/src/db/connection.rs
@@ -1,6 +1,10 @@
-use sea_orm::{Database, DatabaseConnection};
+use sea_orm::{Database, DatabaseConnection, ConnectOptions, DbErr};
 use crate::config::Settings;
 
-pub async fn connect(settings: &Settings) -> Result<DatabaseConnection, sea_orm::DbErr> {
-    Database::connect(&settings.database.url).await
+pub async fn connect(settings: &Settings) -> Result<DatabaseConnection, DbErr> {
+    let mut opt = ConnectOptions::new(settings.database.url.clone());
+    opt.max_connections(settings.database.max_connection);
+    opt.min_connections(1);
+    opt.sqlx_logging(false);
+    Database::connect(opt).await
 }
