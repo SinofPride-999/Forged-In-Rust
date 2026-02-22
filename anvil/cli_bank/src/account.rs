c3a275ae29aa6598a2bd0b3ae8217ae32c5e8f9b
SinofPride-999 <jhaycodes999@gmail.com>
2026-02-22 20:03:07 +0000

    fix(anvil/cli_banks): deposit function

--- anvil/cli_bank/src/account.rs
+++ anvil/cli_bank/src/account.rs
@@ -1,10 +1,9 @@
 pub struct Account {
-    pub id: u32,
     pub balance: f64,
 }
 
 impl Account {
     pub fn new(id: u32, balance: f64) -> Self {
-        Self { id, balance }
+        Self { balance }
     }
 }
--- anvil/cli_bank/src/bank.rs
+++ anvil/cli_bank/src/bank.rs
@@ -7,8 +7,6 @@ use crate::new::{register_new_account};
 pub struct Bank {
     pub accounts: HashMap<u32, Account>,
     pub users: HashMap<u32, User>,
-    pub next_account_id: u32,
-    pub next_user_id: u32,
 }
 
 impl Bank {
@@ -16,36 +14,18 @@ impl Bank {
         Self {
             accounts: HashMap::new(),
             users: HashMap::new(),
-            next_account_id: 0,
-            next_user_id: 0,
         }
     }
 
-    pub fn generate_next_user_id(&mut self) -> u32 {
-        let id = self.next_user_id;
-        self.next_user_id += 1;
-        id
-    }
-
-    pub fn generate_next_account_id(&mut self) -> u32 {
-        let id = self.next_account_id;
-        self.next_account_id += 1;
-        id
-    }
-
-    pub fn add_account(&mut self, initial_balance: f64) -> u32 {
-        let id = self.generate_next_account_id();
-        let account = register_new_account(id, initial_balance);
-
-        self.accounts.insert(id, account);
-
-        id
-    }
-
     pub fn deposit(&mut self, account_id: u32, amount: f64) -> Result<(), String> {
+        if amount <= 0.0 {
+            return Err("Amount must be greater than 0".to_string());
+        }
+
         match self.accounts.get_mut(&account_id) {
             Some(account) => {
-               account.balance += amount;
+                account.balance += amount;
+                println!("{} has been deposited successfully", amount);
                 Ok(())
             }
             None => Err(format!("Account {} not found", account_id)),
